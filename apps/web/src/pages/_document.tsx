a4e0cc9a77a17f59e08785ddf33ace2ae7078d76
__filename <filename@inft.kr>
2023-01-12 21:15:59 +0800

    chore(web): added mui

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,16 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
   render() {
     return (
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-          />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
 
           <link
             rel="stylesheet"
