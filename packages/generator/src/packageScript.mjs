36896ab6c4c225f570e91bea6764fbdad00999de
__filename <filename@inft.kr>
2023-01-20 22:18:52 +0800

    feat(core, packages): added packages, updated tsconfig data

---
+++ packages/generator/src/packageScript.mjs
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+import { program } from 'commander';
+import { createPackage } from '../dist/index.mjs';
+
+program
+  .description(
+    'A script for creating Idle Factory packages. \nThis code used discord.js/script project',
+  )
+  .argument('<name>', 'The name of the new package.')
+  .argument('[description]', 'The description of the new package.');
+program.parse();
+
+const [packageName, description] = program.args;
+
+console.log(`Creating package @idle/${packageName}...`);
+await createPackage(packageName, description);
+console.log('Done!');
